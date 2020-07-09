3fea02e65a55405d8c2ac2085e41e1e91f1a030c
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-07-09 19:06:32 +0700

    Add theme colors

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+
+import { ThemeProvider } from '@xstyled/styled-components'
+
+const theme = {
+  colors: {
+    primaryRed: '#EB5757',
+    primaryGreen: '#27AE60',
+    primaryMango: '#F2994A',
+    darkRed: '#A71313',
+    light: '#ffffff',
+    dark: '#000000',
+  },
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
-);
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
