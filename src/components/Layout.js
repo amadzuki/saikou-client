983f30ef48e3a921698d6424b7163e121e6441aa
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-07-10 22:56:57 +0700

    Implement Layout component

---
+++ src/components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import Hero from './Hero'
+import Footer from './Footer'
+const Layout = () => {
+  return (
+    <>
+      <Hero></Hero>
+      <Footer></Footer>
+    </>
+  )
+}
+
+export default Layout
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import Hero from '../components/Hero'
+import Layout from '../components/Layout'
 
 const Home = () => {
   return (
     <div>
-      <Hero></Hero>
+      <Layout></Layout>
     </div>
   )
 }
