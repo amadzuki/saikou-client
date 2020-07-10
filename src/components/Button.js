6b083da1a8efffc7121f3e2df36b642336049f93
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-07-10 22:56:37 +0700

    Add more prop styling and refactor margin

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,23 +1,28 @@
 import React from 'react'
 import styled from '@xstyled/styled-components'
-import { color, backgroundColor } from '@xstyled/system'
+import { color, backgroundColor, borderColor } from '@xstyled/system'
 
 const ButtonTemplate = styled.a`
   font-family: title;
   font-size: 30px;
   padding: 10 20;
-  margin: 20;
+  margin: 0 20 20 20;
   border: 5px solid #000;
   box-sizing: border-box;
   border-radius: 10px;
   color: lightFont;
+  ${borderColor}
   ${backgroundColor}
   ${color}
 `
 
-const Button = ({ backgroundColor, color, children }) => {
+const Button = ({ borderColor, backgroundColor, color, children }) => {
   return (
-    <ButtonTemplate color={color} backgroundColor={backgroundColor}>
+    <ButtonTemplate
+      color={color}
+      backgroundColor={backgroundColor}
+      borderColor={borderColor}
+    >
       {children}
     </ButtonTemplate>
   )
