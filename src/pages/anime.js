8c6bd1972d169d14b39cac58d776dab77ecac183
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-07-14 23:29:17 +0700

    Implement animeCollection page

--- src/pages/anime.js
+++ src/pages/anime.js
@@ -3,11 +3,32 @@ import styled from '@xstyled/styled-components'
 
 import Layout from '../components/Layout'
 import SearchBar from '../components/SearchBar'
+import Card from '../components/Card'
+
+import CardCovers from '../data/CardCovers.json'
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 70px 0;
+`
 
 const AnimeCollection = () => {
+  const AnimeCovers = CardCovers.filter((cover) => cover.type === 'anime')
   return (
     <Layout backgroundImage="url('/backgrounds/animeCollection.jpg')">
       <SearchBar></SearchBar>
+      <Cards>
+        {AnimeCovers.map((cover, index) => (
+          <Card
+            key={index}
+            imagesrc={cover.imageSrc}
+            imagealt={cover.imageAlt}
+            name={cover.name}
+          />
+        ))}
+      </Cards>
     </Layout>
   )
 }
