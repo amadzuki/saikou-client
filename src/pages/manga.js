372e9b29ece29ad8ded8f2f77139d608784cc8eb
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-07-14 23:29:28 +0700

    Implement mangaCollection page

--- src/pages/manga.js
+++ src/pages/manga.js
@@ -1,12 +1,34 @@
 import React from 'react'
+import styled from '@xstyled/styled-components'
 
 import Layout from '../components/Layout'
 import SearchBar from '../components/SearchBar'
+import Card from '../components/Card'
+
+import CardCovers from '../data/CardCovers.json'
+
+const Cards = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  margin: 70px 0;
+`
 
 const MangaCollection = () => {
+  const MangaCovers = CardCovers.filter((cover) => cover.type === 'manga')
   return (
-    <Layout backgroundImage="url('/backgrounds/manga.jpg')">
+    <Layout backgroundImage="url('/backgrounds/mangaCollection.jpg')">
       <SearchBar></SearchBar>
+      <Cards>
+        {MangaCovers.map((cover, index) => (
+          <Card
+            key={index}
+            imagesrc={cover.imageSrc}
+            imagealt={cover.imageAlt}
+            name={cover.name}
+          />
+        ))}
+      </Cards>
     </Layout>
   )
 }
