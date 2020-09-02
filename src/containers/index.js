4f831a216692cedff0ad13d8bd3b4b16e190cfe9
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-09-02 23:12:33 +0700

    Sort out containers

---
+++ src/containers/index.js
@@ -0,0 +1,19 @@
+import CardContainer from './CardContainer'
+import ItemDetailContainer from './ItemDetailContainer'
+import ItemPicker from './ItemPicker'
+import PrivateRoute from './PrivateRoute'
+import Redirector from './Redirector'
+import RouteAddSlug from './RouteAddSlug'
+import ScrollToTop from './ScrollToTop'
+import TopItemContainer from './TopItemContainer'
+
+export {
+  CardContainer,
+  ItemDetailContainer,
+  ItemPicker,
+  PrivateRoute,
+  Redirector,
+  RouteAddSlug,
+  ScrollToTop,
+  TopItemContainer,
+}
