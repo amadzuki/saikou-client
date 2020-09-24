d2ad522a3829f587777ed5442bb8d543c527fbf6
Ahmad Marzuki <ahmadmarzuki7777@gmail.com>
2020-09-24 17:10:12 +0700

    Add `yarn lint` script

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
   },
   rules: {
     'linebreak-style': ['error', 'unix'],
-    quotes: ['error', 'single', 'avoidEscape'],
+    quotes: ['error', 'single', { avoidEscape: true }],
     semi: ['error', 'never'],
     'jsx-quotes': ['error', 'prefer-single'],
     'react/no-unescaped-entities': ['error', { forbid: ['>', '}'] }],
